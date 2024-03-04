d4516b9984773504c8d5c42aa505b73e0490e8f8
bcjacobs <45748770+bcjacobs@users.noreply.github.com>
2024-03-04 09:18:06 -0500

    Add files via upload

--- jacobs1/sketch.js
+++ jacobs1/sketch.js
@@ -1,58 +1,50 @@
 let audioStarted = false; // needed to get it to work in full screen mode
 
-let lowLevels;
-let highLevels;
-
-let fft;
-
-let mic;
-
-let spectrum;
+var levelsLow;
+var levelsHigh;
 
 function preload(){
   sound = loadSound('drumLoop.mp3');
 }
 
+
+
 function setup() {
-  
+
   getAudioContext().suspend(); // needed to get it to work in full screen mode
-  
-  fullscreen();
+
   
   createCanvas(710, 400, WEBGL);
-  // sound.play();
   
   fft = new p5.FFT();
-  mic = new p5.AudioIn();
-  mic.start()
-  
-  fft.setInput(mic);
+  sound.play();
+  sound.amp(0.2);
 }
 
 function draw() {
-  
-
-  
-  
   background(255);
   
+  let spectrum = fft.analyze();
   
-  spectrum = fft.analyze();
+  if(spectrum[10] > 100){
+    levelsLow = spectrum[10];
+  }
   
-  // console.log(spectrum);
-  
-  // console.log(mic.getLevel());
-  
-  lowLevels = spectrum[10];
-  highLevels = spectrum[500];
-  
-  
+  if(spectrum[200] > 100){
+    levelsHigh = spectrum[200];
+  }
+ 
+  for (let i=0; i<width; i=i+7){
+    line(i-355, -200, i-355, 200);
+  }
+
+
   push();
   translate(-150, 0, 0);
   // rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  torus(lowLevels, 20);
+  torus(levelsLow, 20);
   pop();
   
   push();
@@ -60,8 +52,9 @@ function draw() {
   // rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  torus(highLevels , 20);
+  torus(levelsHigh , 20);
   pop();
+  
 }
 
 function mousePressed() { // needed to get it to work in full screen mode
