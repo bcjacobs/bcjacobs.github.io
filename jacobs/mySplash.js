95bd8871427bb839cb779c9cdadbfbd8c2d8f215
bcjacobs <45748770+bcjacobs@users.noreply.github.com>
2024-04-30 13:52:53 -0400

    Add files via upload

--- jacobs/mySplash.js
+++ jacobs/mySplash.js
@@ -5,26 +5,29 @@ class Splash {
   this.splashBorder = 100;
   fill(255);
   stroke(255, 0, 0)
-  rect(this.splashBorder, this.splashBorder, windowWidth-this.splashBorder*2, windowHeight-this.splashBorder*2);
+  rect(windowWidth/2-250, 
+       this.splashBorder, 
+       500, 
+       windowHeight-this.splashBorder*2);
   // draw a rectangle like this in a 3D enviornment
   // rect(this.splashBorder-(windowWidth/2), this.splashBorder-(windowHeight/2), windowWidth-this.splashBorder*2, windowHeight-this.splashBorder*2);
   fill(0, 0, 222);
   strokeWeight(3)
    
-  line(windowWidth-this.splashBorder-40, this.splashBorder+20,windowWidth-this.splashBorder-20, this.splashBorder+40)
-   line(windowWidth-this.splashBorder-20, this.splashBorder+20,windowWidth-this.splashBorder-40, this.splashBorder+40)
+  line(windowWidth/2+200, this.splashBorder+20,windowWidth/2+220, this.splashBorder+40)
+   line(windowWidth/2+220, this.splashBorder+20,windowWidth/2+200, this.splashBorder+40)
    
   this.title = createDiv("2D Sequencer");
   this.title.style('color:deeppink');
   this.title.style('font-family: Arial, Helvetica, sans-serif');
-  this.title.position(this.splashBorder+20, this.splashBorder+20);
+  this.title.position(windowWidth/2-200, this.splashBorder+40);
   
   this.name = createDiv("Bryan Jacobs");
-  this.name.position(this.splashBorder+20, this.splashBorder+60);
+  this.name.position(windowWidth/2-200, this.splashBorder+80);
   
   this.info = createDiv("This is a 2D sequencer operating at two different tempi. Each glowing orb can be moved around the space. <p> <a href=https://editor.p5js.org/bcjacobs/sketches/SijjGsY40y>view source code</a>");
   
-  this.info.position(this.splashBorder+20, this.splashBorder+100);
+  this.info.position(windowWidth/2-200, this.splashBorder+120);
   this.info.size(windowWidth-this.splashBorder*2-50, windowHeight-this.splashBorder*2-50)
    
 
@@ -32,8 +35,8 @@ class Splash {
 }
   
   update(){
-       if(mouseX > windowWidth-this.splashBorder-40 && 
-          mouseX < windowWidth-this.splashBorder-20 
+       if(mouseX > windowWidth/2+200 && 
+          mouseX < windowWidth/2+220 
           && mouseY < this.splashBorder+40 
           && mouseY > this.splashBorder+20
      ){
